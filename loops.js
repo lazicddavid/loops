2aa8590fddcccad99958f681c7e3b31cfcd42b62
David Lazic <lazic.ddavid@gmail.com>
2025-08-20 22:54:20 +0200

    Added body

--- loops.js
+++ loops.js
@@ -5,3 +5,46 @@ for (let i = 1; i <= 10; i++) {
 }
 
 console.log(zbir);
+
+console.log("----------------------");
+
+for (let i = 0; i <= 20; i += 2) {
+  console.log(i);
+}
+
+console.log("----------------------");
+
+let ljudi = [
+  { id: 1, ime: "Strahinja" },
+  { id: 2, ime: "Nemanja" },
+  { id: 3, ime: "Milica" },
+  { id: 4, ime: "Nikola" },
+];
+
+for (let i = 0; i < ljudi.length; i++) {
+  console.log(ljudi[i].ime);
+}
+
+console.log("----------------------");
+
+let brojevi = [5, 12, 8, 20, 3];
+let zbir = 0;
+
+for (let i = 0; i < brojevi.length; i++) {
+  zbir += brojevi[i];
+}
+
+console.log(zbir);
+
+console.log("----------------------");
+
+let brojevi = [5, 12, 8, 20, 3];
+
+let max = brojevi[0];
+for (let i = 1; i < brojevi.length; i++) {
+  if (brojevi[i] > max) {
+    max = brojevi[i];
+  }
+}
+
+console.log(max);
