ef8eb30f3f14e3fffba511964621fee7549de5ad
David Lazic <lazic.ddavid@gmail.com>
2025-08-22 22:00:46 +0200

    Added let

--- loops.js
+++ loops.js
@@ -5,5 +5,7 @@ Dat ti je niz stringova:
 let recenice = ["Ovo je prva rečenica", "Druga rečenica ovde", "Treća"];
 Napiši petlju koji sabira ukupan broj reči u svim rečenicama.Treba da console loguije rezultat i da ga vrati.*/
 
-let brojReci = "";
-let ukupno = 0;
+function brojReci() {
+  let recenice = ["Ovo je prva recenica", "Druga recenica ovde", "Treca"];
+  let ukupno = 0;
+}
