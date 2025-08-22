cb338e0d42330ae5d7d749604b276f0288e7e507
David Lazic <lazic.ddavid@gmail.com>
2025-08-22 21:58:43 +0200

    Added let

--- loops.js
+++ loops.js
@@ -1,50 +1,9 @@
-let zbir = 0;
+/*
+1. Broj reči u nizu stringova
+Dat ti je niz stringova:
 
-for (let i = 1; i <= 10; i++) {
-  zbir = zbir + i;
-}
+let recenice = ["Ovo je prva rečenica", "Druga rečenica ovde", "Treća"];
+Napiši petlju koji sabira ukupan broj reči u svim rečenicama.Treba da console loguije rezultat i da ga vrati.*/
 
-console.log(zbir);
-
-console.log("----------------------");
-
-for (let i = 0; i <= 20; i += 2) {
-  console.log(i);
-}
-
-console.log("----------------------");
-
-let ljudi = [
-  { id: 1, ime: "Strahinja" },
-  { id: 2, ime: "Nemanja" },
-  { id: 3, ime: "Milica" },
-  { id: 4, ime: "Nikola" },
-];
-
-for (let i = 0; i < ljudi.length; i++) {
-  console.log(ljudi[i].ime);
-}
-
-console.log("----------------------");
-
-let brojevi = [5, 12, 8, 20, 3];
-let zbir = 0;
-
-for (let i = 0; i < brojevi.length; i++) {
-  zbir += brojevi[i];
-}
-
-console.log(zbir);
-
-console.log("----------------------");
-
-let brojevi = [5, 12, 8, 20, 3];
-
-let max = brojevi[0];
-for (let i = 1; i < brojevi.length; i++) {
-  if (brojevi[i] > max) {
-    max = brojevi[i];
-  }
-}
-
-console.log(max);
+let brojReci = "";
+let ukupno = 0;
